2a648d68409442c471d58a71a1ad41a11524b5c5
Amélie <1855877@cegepmontpetit.ca>
2024-09-20 17:26:01 -0400

    Labo complété

--- signalr.backend/signalr.backend/Hubs/ChatHub.cs
+++ signalr.backend/signalr.backend/Hubs/ChatHub.cs
@@ -36,11 +36,25 @@ namespace signalr.backend.Hubs
             _context = context;
         }
 
+        /// <summary>
+        /// Liste des utilisateurs connectés
+        /// </summary>
+        /// <returns>Liste des utilisateurs connecté</returns>
+        private async Task UserList()
+        {
+            //All = tous, "UsersList", ce qu'il y a côté client, UserHandler = permet d'avoir la liste des utilisateurs connectés
+            await Clients.All.SendAsync("UsersList", UserHandler.UserConnections.ToList());
+        }
+
         public async override Task OnConnectedAsync()
         {
             UserHandler.UserConnections.Add(CurentUser.Email!, Context.UserIdentifier);
-            
-            // TODO: Envoyer des message aux clients pour les mettre à jour
+
+            // TODO: Envoyer des messages aux clients pour les mettre à jour
+            //liste des utilisateurs
+            await UserList();
+            //liste des channels
+            await Clients.All.SendAsync("ChannelsList", _context.Channel.ToList());
         }
 
         public async override Task OnDisconnectedAsync(Exception? exception)
@@ -48,8 +62,9 @@ namespace signalr.backend.Hubs
             // Lors de la fermeture de la connexion, on met à jour notre dictionnary d'utilisateurs connectés
             KeyValuePair<string, string> entrie = UserHandler.UserConnections.SingleOrDefault(uc => uc.Value == Context.UserIdentifier);
             UserHandler.UserConnections.Remove(entrie.Key);
-            
+
             // TODO: Envoyer un message aux clients pour les mettre à jour
+            await UserList();
         }
 
         public async Task CreateChannel(string title)
@@ -58,6 +73,7 @@ namespace signalr.backend.Hubs
             await _context.SaveChangesAsync();
 
             // TODO: Envoyer un message aux clients pour les mettre à jour
+            await Clients.All.SendAsync("ChannelsList", _context.Channel.ToListAsync());
         }
 
         public async Task DeleteChannel(int channelId)
@@ -71,15 +87,55 @@ namespace signalr.backend.Hubs
             }
             string groupName = CreateChannelGroupName(channelId);
             // Envoyer les messages nécessaires aux clients
+
+            //envoi d'un message afin d'avertir les utilisateurs que le canal a été détruit
+            await Clients.Group(groupName).SendAsync("NewMessage", "[" + channelId + "]" + " a été détruit");
+            //cherche du côté client la fonction LeaveChannel
+            await Clients.Group(groupName).SendAsync("LeaveChannel");
+            //mettre à jour pour tous la liste des canaux existants
+            await Clients.All.SendAsync("ChannelsList", _context.Channel.ToList());
         }
 
+        /// <summary>
+        /// Permet de se joindre à un canal
+        /// </summary>
+        /// <param name="oldChannelId">ancien id du canal </param>
+        /// <param name="newChannelId">nouvel id du canal</param>
+        /// <returns></returns>
         public async Task JoinChannel(int oldChannelId, int newChannelId)
         {
             string userTag = "[" + CurentUser.Email! + "]";
 
             // TODO: Faire quitter le vieux canal à l'utilisateur
 
+            if (oldChannelId > 0)
+            {
+                //créer un nom de group avec l'id du canal
+                string oldGroupName = CreateChannelGroupName(oldChannelId);
+                //trouver le canal
+                Channel channel = _context.Channel.Find(oldChannelId);
+                //initialiser un message
+                string message = userTag + " quitte : " + channel.Title;
+                //envoyer le message aux utilisateurs concernés
+                await Clients.Group(oldGroupName).SendAsync("NewMessage", message);
+                //Gérer le groupe -- retirer du groupe
+                //besoin de l'id de la connexion et du nom en string du groupe
+                await Groups.RemoveFromGroupAsync(Context.ConnectionId, oldGroupName);
+            }
+            
+
             // TODO: Faire joindre le nouveau canal à l'utilisateur
+            if(newChannelId > 0)
+            {
+                string newGroupName = CreateChannelGroupName(newChannelId);
+                await Groups.AddToGroupAsync(Context.ConnectionId, newGroupName);
+
+                Channel channel = _context.Channel.Find(newChannelId);
+               
+                //message
+                string message = userTag + " a joint : " + channel.Title;
+                await Clients.Group(newGroupName).SendAsync("NewMessage", message);
+            }
         }
 
         public async Task SendMessage(string message, int channelId, string userId)
@@ -87,10 +143,15 @@ namespace signalr.backend.Hubs
             if (userId != null)
             {
                 // TODO: Envoyer le message à cet utilisateur
+                string messageTag = "[De " + CurentUser.Email! + "]" + message;
+                await Clients.User(userId).SendAsync("NewMessage", messageTag);
             }
             else if (channelId != 0)
             {
                 // TODO: Envoyer le message aux utilisateurs connectés à ce canal
+                string groupName = CreateChannelGroupName(channelId);
+                Channel channel = _context.Channel.Find(channelId);
+                await Clients.Group(groupName).SendAsync("NewMessage", "[" + channel.Title + "]" + message);
             }
             else
             {
--- signar.ng/src/app/components/chat/chat.component.ts
+++ signar.ng/src/app/components/chat/chat.component.ts
@@ -47,12 +47,19 @@ export class ChatComponent  {
     });
 
     // TODO: Écouter le message pour mettre à jour la liste de channels
+    this.hubConnection.on('ChannelsList', (data) => {
+      this.channelsList = data;
+    });
 
     this.hubConnection.on('NewMessage', (message) => {
       this.messages.push(message);
+      console.log(this.messages);
     });
 
     // TODO: Écouter le message pour quitter un channel (lorsque le channel est effacé)
+    this.hubConnection.on('LeaveChannel', (message) => {
+      this.selectedChannel = null;
+    });
 
     // On se connecte au Hub
     this.hubConnection
@@ -82,12 +89,15 @@ export class ChatComponent  {
 
   createChannel(){
     // TODO: Ajouter un invoke
+    this.hubConnection!.invoke('CreateChannel', this.newChannelName);
   }
 
   deleteChannel(channel: Channel){
     // TODO: Ajouter un invoke
+    this.hubConnection!.invoke('DeleteChannel', this.selectedChannel);
   }
 
+  //fonction côté client client afin de quitter un canal
   leaveChannel(){
     let selectedChannelId = this.selectedChannel ? this.selectedChannel.id : 0;
     this.hubConnection!.invoke('JoinChannel', selectedChannelId, 0);
